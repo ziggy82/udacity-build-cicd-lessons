bd37537595edeaada6ee5d72e1523c4fb34406a8
ziggy82 <zaheir786@hotmail.com>
2025-09-29 17:34:44 -0400

    terraform practice

--- lesson-4-demos/terraform-demo/new_bucket.tf
+++ lesson-4-demos/terraform-demo/new_bucket.tf
@@ -16,7 +16,7 @@ resource "aws_s3_bucket" "udabucket" {
 
 # Create an EC2 instance
 resource "aws_instance" "ec2_instance" {
-  ami           = "ami-04a0ae173da5807d3"
+  ami           = "ami-08982f1c5bf93d976"
   instance_type = "t2.micro"
 
   tags = {
